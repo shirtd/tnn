5f4cd20c1ce09d220b8f7baa91e048459820c530
shirt <kirk@beedle.gar.den>
2018-08-18 21:43:56 -0400

    test

--- src/tda.py
+++ src/tda.py
@@ -26,6 +26,14 @@ def fmap(r, d):
 def persist(data, X, c):
     return ripser(data['X'][X[c]].T, do_cocycles=True)
 
+def pmap(f, x):
+    pool = Pool()
+    y = pool.map(f, x)
+    pool.close()
+    pool.join()
+    return y
+
+
 def get_masks(data, dims=DIMS):
     dims = range(dims+1)
     C = np.unique(data['y'])
@@ -33,10 +41,11 @@ def get_masks(data, dims=DIMS):
     sprint(2, '| computing persistence diagrams')
     warnings.filterwarnings("ignore")
     f = partial(persist, data, X)
-    pool = Pool()
-    R = dict(zip(C, pool.map(f, C)))
-    pool.close()
-    pool.join()
+    R = dict(zip(C, pmap(f, C)))
+    # pool = Pool()
+    # R = dict(zip(C, pool.map(f, C)))
+    # pool.close()
+    # pool.join()
     # R = {c : ripser(data['X'][X[c]].T, do_cocycles=True) for c in C}
     sprint(2, '| retrieving cocycles')
     B = {d : {c : R[c]['dgms'][d] for c in C} for d in dims}
@@ -50,6 +59,9 @@ def to_mask(dgm, k=100, l=0.0, f=lambda x: x ** 2, n=28*28):
 def make_masks(dgm, k=100, l=0.0, f=lambda x: x ** 2):
     return np.vstack(map(lambda x: to_mask(x, k, l, f), dgm)).reshape(-1, 28, 28)
 
+def masks_fun(dgms, k, l, f, c):
+    return make_masks(dgms[c], k ,l, f)
+
 def fmask(X):
     x = np.array(np.sum(X, axis=0), dtype=float)
     num, den = x - x.min(), x.max() - x.min()
@@ -62,4 +74,11 @@ def fmask(X):
     # return x if mx == 0 else x / mx if mx - mn == 0 else (x - mx) / (mx - mn)
 
 def build_masks(jdict, dim=DIM, k=100, l=0.0, f=lambda x: x ** 2):
-    return {c : make_masks(jdict['diagrams'][dim][c], k ,l, f) for c in jdict['keys']}
+    f = partial(masks_fun, jdict['diagrams'][dim], k ,l, f)
+    return dict(zip(jdict['keys'], pmap(f, jdict['keys'])))
+    # pool = Pool()
+    # d = dict(zip(jdict['keys'], pool.map(f, jdict['keys'])))
+    # pool.close()
+    # pool.join()
+    # return d
+    # return {c : make_masks(jdict['diagrams'][dim][c], k ,l, f) for c in jdict['keys']}
