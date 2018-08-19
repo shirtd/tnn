cf9de2168cbb2b826ab50d5a81f3c05bee7d0b08
shirt <kirk@beedle.gar.den>
2018-08-19 02:05:53 -0400

    memory

--- net.py
+++ net.py
@@ -13,7 +13,8 @@ def main(args, masks=[], jdict = {}):
         for k in sorted(args.__dict__.keys()):
             sprint(2, "({}): {}".format(k, args.__dict__[k]))
     if len(masks) == 0 and not args.no_mask:
-        fin = os.path.join(args.data, args.load + '%d.pkl' % args.k)
+        # fin = os.path.join(args.data, args.load + '%d.pkl' % args.k)
+        fin = os.path.join(args.data, args.load + '.pkl')
         if not args.save and os.path.exists(fin) and len(args.load) > 0:
             sprint(1, '[ loading %s' % fin)
             with open(fin, 'r') as f:
@@ -21,7 +22,7 @@ def main(args, masks=[], jdict = {}):
         else:
             train = get_data('train', args.dir)
             test = get_data('test', args.dir)
-            sprint(1, '[ getting masks in dimension 0-%d' % args.dims)
+            sprint(1, '[ getting masks in dimension 0:%d' % args.dims)
             jdict = get_persist(train, args.dims)
             sprint(1, '[ building masks in dimension %d' % args.dim)
             jdict['masks'] = get_masks(jdict, args.dim, args.k)
@@ -29,7 +30,8 @@ def main(args, masks=[], jdict = {}):
                 if not os.path.exists(args.data):
                     sprint(2, '! creating directory %s' % args.data)
                     os.mkdir(args.data)
-                fout = os.path.join(args.data, args.fout + '%d.pkl' % args.k)
+                # fout = os.path.join(args.data, args.fout + '%d.pkl' % args.k)
+                fout = os.path.join(args.data, args.fout + '.pkl')
                 sprint(2, '| writing to %s' % fout)
                 with open(fout, 'w') as f:
                     pkl.dump(jdict, f)
